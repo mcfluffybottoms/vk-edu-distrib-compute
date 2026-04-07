cad102dae864af0e4a072a6c33db96eca47b9c3b
Алтунина Екатерина <166242132+mcfluffybottoms@users.noreply.github.com>
2026-04-07 18:23:10 +0300

    File Dao

--- src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
@@ -27,13 +27,15 @@ import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.support.ParameterDeclarations;
 import org.junit.platform.commons.util.ReflectionUtils;
 
-import company.vk.edu.distrib.compute.mcfluffybottoms.KVServiceFactoryImpl;
+import company.vk.edu.distrib.compute.mcfluffybottoms.FileKVServiceFactory;
+import company.vk.edu.distrib.compute.mcfluffybottoms.InMemoryKVServiceFactory;
 
 public class KVServiceFactoryArgumentsProvider implements ArgumentsProvider {
 
     private final Set<Class<? extends KVServiceFactory>> factories = Set.of(
         KVServiceFactorySimple.class,
-        KVServiceFactoryImpl.class,
+        InMemoryKVServiceFactory.class,
+        FileKVServiceFactory.class,
         AndecoKVServiceFactory.class,
         MyKVServiceFactory.class,
         InMemoryKVServiceFactory.class,
