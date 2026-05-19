27774f67f1e4be9ab698175795e3532da32680be
Алтунина Екатерина <muchashamedofyou@gmail.com>
2026-05-19 16:08:36 +0300

    delete file

--- mcfluffybottoms-results/task1/put_request.lua
+++
@@ -1,8 +0,0 @@
-wrk.method = "PUT"
-wrk.headers["Content-Type"] = "application/json"
-
-request = function()
-    local id = math.random(1, 1000000)
-    local body = string.format('{"value": "data_%d"}', id)
-    return wrk.format("PUT", "/v0/entity/" .. id, nil, body)
-end
